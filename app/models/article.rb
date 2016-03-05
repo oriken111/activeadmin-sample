6975458f9710e42da51e8cf7441931f1c800190e
oriken111 <oriken111@gmail.com>
2016-03-05 09:25:09 +0900

    Changing counts in production

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  scope :group_by_this_month,   -> { group("date_trunc('month', created_at) ") }
   validates :content, presence: true, inclusion: { in: %w(evernote.com) }
 end
